e27a6aba1ee3e3ccd6d9e987e65ccc854896620d
Saurabh-Singh2005 <singhsaurabh6262@gmail.com>
2026-08-21 23:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (11.78%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
 #include <iostream>
-
-int getLength(int x) {
-    if (x == 0) return 1;
-    
-    int count = 0;
-    while (x != 0) {
-        x /= 10;
-        count++;
-    }
-    return count;
-}
-
     bool isPalindrome(int x) {
-        int n=getLength(x);
-        std::string s = std::to_string(x);
+         std::string s = std::to_string(x);
+        int n=s.size();
+    
         int l=0;
         int r=n-1;
         while(l<=r){
