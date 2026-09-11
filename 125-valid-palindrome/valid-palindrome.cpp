53666514a861d967ba3506863690cf840393c38c
Saurabh-Singh2005 <singhsaurabh6262@gmail.com>
2026-09-11 18:26:43 +0530

    Time: 7 ms (5.28%) | Memory: 9.9 MB (52.53%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       std::transform(s.begin(),s.end(),s.begin(),::tolower);
+       int l=0,r=s.size()-1;
+       
+       while(l<=r){
+        if(!isalnum(s[l])){ l++; continue;}
+        if(!isalnum(s[r])){ r--; continue;}
+        if(s[l]!=s[r]){return false;}
+            l++;
+            r--;
+            } 
+       return true;
+    }
+};
